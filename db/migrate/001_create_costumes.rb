c64a5d26e71a42f3cebaaa4d6231856fb71e0f8e
Grace Choi <oiograceoio@gmail.com>
2019-03-22 21:01:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,11 @@
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :image_url
-      t.string :size
-      t.timestamps
+      t.string :size 
+      t.timestamps 
     end
   end
 end
